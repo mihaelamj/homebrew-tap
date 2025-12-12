62280c6ccdfb066386121faaf8b8adf9ec23d5f2
Mihaela Mihaljevic <mihaelamj@me.com>
2025-12-12 03:06:46 +0100

    chore: bump cupertino to 0.6.0

--- Formula/cupertino.rb
+++ Formula/cupertino.rb
@@ -1,9 +1,9 @@
 class Cupertino < Formula
   desc "Apple Documentation MCP Server - Search Apple docs, Swift Evolution, and sample code"
   homepage "https://github.com/mihaelamj/cupertino"
-  url "https://github.com/mihaelamj/cupertino/releases/download/v0.5.0/cupertino-v0.5.0-macos-universal.tar.gz"
-  sha256 "9eac5e49e5c5497afb3c14da96740db1968249433bd913102891d65cfe6b6fa0"
-  version "0.5.0"
+  url "https://github.com/mihaelamj/cupertino/releases/download/v0.6.0/cupertino-v0.6.0-macos-universal.tar.gz"
+  sha256 "ba8a23a91d396bff7845a62fb375cbc57852518db7fbb68ef1bd14726666b1f4"
+  version "0.6.0"
   license "MIT"
 
   depends_on :macos
@@ -20,6 +20,6 @@ class Cupertino < Formula
   end
 
   test do
-    assert_match "0.5.0", shell_output("#{bin}/cupertino --version")
+    assert_match "0.6.0", shell_output("#{bin}/cupertino --version")
   end
 end
